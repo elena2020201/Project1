95adba1beb5dd1688a45f56e688ba6a2e52b01fd
elena2020201 <elenarus1992@mail.ru>
2021-05-24 18:13:25 +0300

    Добавьте файлы проекта.

---
+++ больш меньш конс/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace больш_меньш_конс
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Введите число");
+            string sum = Console.ReadLine();
+            int a = int.Parse(sum);
+            int b = a - 1;
+            int c = a + 1;
+            Console.WriteLine("Последовательность " + b + "  " + a + "  " + c);
+            Console.ReadLine();
+        }
+    }
+}
